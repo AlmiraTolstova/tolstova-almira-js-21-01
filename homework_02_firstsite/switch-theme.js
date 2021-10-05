c33974bbdbeab912678a8ee47395eccb7d7397cc
AlmiraTolstova <almiratolstova@gmail.com>
2021-10-05 21:01:21 +0300

    Написан скрипт для смены темы switch-theme.js

--- homework_02_firstsite/switch-theme.js
+++ homework_02_firstsite/switch-theme.js
@@ -2,43 +2,48 @@ const buttonSwitchTheme = document.getElementById('switch-theme');
 // Добавим обработчик для события "click", предоставив callback.
 // Теперь по клику на элемент будет всплывать подсказка.
 
-let arrayLightTheme=['.white-text__light-theme', '.black-text__light-theme','.bg-white__light-theme','.bg-gray__light-theme','.bg-mgray__light-theme','.btn__light-theme'];
-let arrayDarkTheme=['.black-text__dark-theme','.white-text__dark-theme','.bg-black__dark-theme','.bg-gray__dark-theme','.bg-lgray__dark-theme','.btn__dark-theme']
+let arrayLightTheme=['white-text__light-theme', 'black-text__light-theme','bg-white__light-theme','bg-gray__light-theme','bg-mgray__light-theme','btn__light-theme'];
+let arrayDarkTheme=['black-text__dark-theme','white-text__dark-theme','bg-black__dark-theme','bg-gray__dark-theme','bg-green__dark-theme','btn__dark-theme']
 
 let currentTheme=false; //false - light theme, true - dark theme
 
 buttonSwitchTheme.addEventListener('click', function (event) {
     //document.getElementsByClassName("white-text__light-theme").classList.toggle("black-text__dark-theme");
     console.log(currentTheme)
-    if (currentTheme==false)
-    {
-        let elementsLightTheme=document.querySelectorAll(".white-text__light-theme, .black-text__light-theme");
-        console.log(elementsLightTheme.length)
-        for(i=0;i<elementsLightTheme.length;i++){
-            console.log(elementsLightTheme[i], "номер объекта: ", i);
-
-            elementsLightTheme[i].classList.add("black-text__dark-theme");
-            elementsLightTheme[i].classList.remove("white-text__light-theme");
-
-        }
-
-        currentTheme=true;
-    }
-    else
-    {
-        let elementsLightTheme=document.querySelectorAll(".black-text__dark-theme");
-        console.log(elementsLightTheme.length)
-        for(i=0;i<elementsLightTheme.length;i++){
-            console.log(elementsLightTheme[i],"номер объекта: ", i);
-
-            elementsLightTheme[i].classList.add("white-text__light-theme");
-            elementsLightTheme[i].classList.remove("black-text__dark-theme");
-
-        }
-        currentTheme=false;
-    }
-
-
+    currentTheme=makeSwitchSelector(arrayLightTheme, arrayDarkTheme, currentTheme);
 
 });
 
+
+function makeSwitchSelector(arrayLightTheme, arrayDarkTheme, currentTheme){
+    if (currentTheme==false)
+    {
+        for(i=0;i<arrayLightTheme.length;i++){
+            let elementsLightTheme=document.querySelectorAll('.'+arrayLightTheme[i]);
+            console.log(elementsLightTheme.length)
+            for(j=0;j<elementsLightTheme.length;j++){
+                    //console.log(elementsLightTheme[j], "номер объекта: ", j);
+                    elementsLightTheme[j].classList.remove(arrayLightTheme[i]);
+                    elementsLightTheme[j].classList.add(arrayDarkTheme[i]);
+
+            }
+        }
+        currentTheme=true;
+        return currentTheme;
+    }
+    else
+    {
+        for(i=0;i<arrayDarkTheme.length;i++){
+             let elementsDarkTheme=document.querySelectorAll('.'+arrayDarkTheme[i]);
+             console.log(elementsDarkTheme.length)
+             for(j=0;j<elementsDarkTheme.length;j++){
+                 //console.log(elementsDarkTheme[j],"номер объекта: ", j);
+
+                 elementsDarkTheme[j].classList.add(arrayLightTheme[i]);
+                 elementsDarkTheme[j].classList.remove(arrayDarkTheme[i]);
+             }
+        }
+        currentTheme=false;
+        return currentTheme;
+    }
+}
