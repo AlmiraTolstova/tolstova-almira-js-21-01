acb85c4a696b210f618dfb651acad5d4187abfd3
AlmiraTolstova <almiratolstova@gmail.com>
2021-10-16 23:21:37 +0300

    форматирование кода

--- homework_05/index.js
+++ homework_05/index.js
@@ -158,80 +158,72 @@ console.log(returnArray(array8, 2, 3));
 // Вспомогательная функция, которая создаёт объект для хранения данных
 const buildCharObject = str => {
     const charObj = {}
-    for(let char of str.replace(/[^\w]/g).toLowerCase()) {
-        // Если объект уже содержит пару ключ-значение равную значению в цикле,
-        // увеличиваем значение на 1, в противном случае добавляем букву в качестве ключа
-        // и 1 в качестве значения
+    for (let char of str.replace(/[^\w]/g).toLowerCase()) {
         charObj[char] = charObj[char] + 1 || 1
     }
     return charObj
 }
-// Главная функция
+
 const anagram = (strA, strB) => {
-    // Создаём объект для хранения strA
+
     const aCharObject = buildCharObject(strA)
-    // Создаём объект для хранения strB
     const bCharObject = buildCharObject(strB)
-    // Сравниваем количество ключей в обоих объектах
-    // (анаграммы должны иметь одинаковое количество букв)
-    if(Object.keys(aCharObject).length !== Object.keys(bCharObject).length) {
+    if (Object.keys(aCharObject).length !== Object.keys(bCharObject).length) {
         return false
     }
-    // Если оба объекта имеют одинаковое количество ключей, мы можем быть уверены,
-    // что обе строки имеют одинаковое количество символов. Теперь мы можем сравнить
-    // оба объекта, чтобы увидеть, имеют ли они одинаковые буквы в одинаковом количестве
-    for(let char in aCharObject) {
-        if(aCharObject[char] !== bCharObject[char]) {
+    for (let char in aCharObject) {
+        if (aCharObject[char] !== bCharObject[char]) {
             return false
         }
     }
-    // Если проверка успешна, строки являются анаграммами — возвращаем true
+    // Если проверка успешна — возвращаем true
     return true
 }
 
-console.log(anagram('ротонда','торнадо'));
+console.log(anagram('ротонда', 'торнадо'));
 
 
 //10. Создать объект, выводящий в консоль все ключи и значения объекта в формате "ключ: значение" через запятую
 // (считать, что значением ключа объекта не может быть объектом или массивом, содержащими объекты) сама функция в консоль выводиться не должна.
 
-const user={
-    name:"Almira",
-    email:"almiratolstova@gmail.com",
-    login:"Almira1993"
+const user = {
+    name: "Almira",
+    email: "almiratolstova@gmail.com",
+    login: "Almira1993"
 }
 
 Object.keys(user).forEach(key => console.log(`key:${key},value:${user[key]}`));
-Object.entries(user).forEach(entry=>console.log(`key:${entry[0]},value:${entry[1]}`))
+Object.entries(user).forEach(entry => console.log(`key:${entry[0]},value:${entry[1]}`))
 
 
 //11. Создать функцию-конструктор для объекта, содержащего методы serProp (установить значение свойства), метод принимает ключь (строка),
 // значение (произвольное) и объект со свойствами writable, configurable, enumerable (разрешение перезаписи свойства,
 // разрешение перечисления свойства и разрешение удаления свойства). Если какое-то из свойств в объекте отсутствует, действие должно быть разрешено
 
-function setProp(key, props){
+function setProp(key, props) {
     console.log(`key:${key},props:${props.configurable}`);
     //this.setProp=()=>function (){
-        let writ, conf, enu;
-        if(props.writable!=undefined) writ=props.writable
-        else writ=true;
-        if(props.configurable!=undefined) conf=props.configurable
-        else conf=true;
-        if(props.enumerable!=undefined) enu=props.enumerable
-        else enu=true;
-        console.log("свойства изнутри",writ, conf, enu);
-        Object.defineProperties(this, {
-                name:{
-                    value:key,
-                    writable:writ,
-                    configurable:conf,
-                    enumerable:enu
-                }
+    let writ, conf, enu;
+    if (props.writable != undefined) writ = props.writable
+    else writ = true;
+    if (props.configurable != undefined) conf = props.configurable
+    else conf = true;
+    if (props.enumerable != undefined) enu = props.enumerable
+    else enu = true;
+    console.log("свойства изнутри", writ, conf, enu);
+    Object.defineProperties(this, {
+            name: {
+                value: key,
+                writable: writ,
+                configurable: conf,
+                enumerable: enu
             }
-        )
+        }
+    )
     //}
 
 }
-const user1=new setProp("Oleg",{writable:true, configurable:false});
+
+const user1 = new setProp("Oleg", {writable: true, configurable: false});
 //user1.setProp();
 console.log(Object.getOwnPropertyDescriptors(user1));
