f8538d9f7f2724dce0d88078fefe5a41a3fa17f8
AlmiraTolstova <almiratolstova@gmail.com>
2021-10-20 20:51:55 +0300

    решение 3 задачи

--- homework_06/index.js
+++ homework_06/index.js
@@ -3,9 +3,9 @@
 // Модернизировать написанную функцию, добавив кэширование (функция ""запоминает"" входной параметр и вычесленное значение,
 // при следующем вызыве с этим же параметром, функция не вычисляет значение, а возвращает из памяти)
 const fibonacci = n => {
-    if(n <= 1){
+    if (n <= 1) {
         return n;
-    }else{
+    } else {
         return fibonacci(n - 1) + fibonacci(n - 2);
     }
 }
@@ -20,3 +20,51 @@ const fib = memoize(fibonacci);
 console.log(fib(6))
 console.log(fib(7))
 
+//3. Разработать рекурсивную функцию, принимающую массив, содержащий массивы из двух элементов, в каждом из которых первый элемент является строкой,
+// а второй строкой, числом, логическим значением, объектом, или таким же массивом. Функция должна преобразовать массив в объект.
+
+//Пример входного значения:[[“name”: “Anna”], [“age”: 22],[“pets”: [[“dog”: “Faust”],[“cat”: “Balthazar”]]]]
+// Пример возвращаемого объекта:{name: “Anna”,age: 22,pets: {dog: “Faust”,cat: “Balthazar”}}
+//     ["pets",[["dog", "Faust"],["cat", "Balthazar"]]]
+
+//Решение задачи через цикл
+const family=[["name", "Anna"], ["age", 22], ["pets",[["dog", "Faust"],["cat", "Balthazar"]]]]
+function sumFamily(family){
+    const newArray={};
+    for(let i=0; i<=family.length-1;i++){
+        if(typeof(family[i][1])!="object"){
+            newArray[family[i][0]]=family[i][1]
+        }else {
+            const subArray={};
+            for(let j=0; j<=family[i][1].length-1;j++){
+                subArray[family[i][1][j][0]]=family[i][1][j][1];
+            }
+            newArray[family[i][0]]=subArray;
+        }
+    }
+    return newArray;
+}
+console.log(sumFamily(family));
+
+//Решение задачи через метод
+const map = new Map([["name", "Anna"], ["age", 22], ["pets",[["dog", "Faust"],["cat", "Balthazar"]]]]);
+const family1=[["name", "Anna"], ["age", 22], ["pets",[["dog", "Faust"],["cat", "Balthazar"]]]];
+function  sumArray(family){
+    const newArray = Object.fromEntries(family);
+    return console.log(newArray);
+}
+sumArray(family1);
+
+
+//Решение задачи через рекурсию
+console.log(Object.fromEntries([["name", "Anna"]]));
+let i=0;
+function frec(family, i){
+    const NA= Object.fromEntries([family[i]]);
+    i++;
+    if(i<family.length){
+        frec(family, i);
+    }
+    return console.log(NA);
+}
+frec(family1, i);
