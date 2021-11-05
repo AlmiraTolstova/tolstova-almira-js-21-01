6c42eb903e7572d344303c121f06efeb0fa31799
AlmiraTolstova <almiratolstova@gmail.com>
2021-11-05 14:50:44 +0300

    Переписанный код на ts

--- homework_11/table_tp/src/main.ts
+++ homework_11/table_tp/src/main.ts
@@ -1,6 +1,4 @@
 // Функции, возвращающие блоки (DOM=-элементы)
-Object.defineProperty(exports, "__esModule", { value: true });
-
 export function createApiPoints(obj) {
     const result = document.createElement('div')
     Object.keys(obj).forEach(key => {
@@ -11,26 +9,24 @@ export function createApiPoints(obj) {
     return result;
 }
 
-
-const createFilm = (name, height, mass, gender) => {
+const createFilm = (name:string, height:number, mass:number, gender:string) => {
     const tr = document.createElement('tr');
-    //tr.append(document.createTextNode(`${name} -`))
     const tdName = document.createElement('td')
     const tdheight = document.createElement('td')
     const tdmass = document.createElement('td')
     const tdgender = document.createElement('td')
     tdName.innerHTML = name;
     tr.append(tdName)
-    tdheight.innerHTML =height;
+    tdheight.innerHTML =height.toString();
     tr.append(tdheight)
-    tdmass.innerHTML =mass;
+    tdmass.innerHTML =mass.toString();
     tr.append(tdmass)
     tdgender.innerHTML =gender;
     tr.append(tdgender)
     return tr;
 }
 
-export const createPeopleList = (peopleArray) => {
+export const createPeopleList = (peopleArray:any ) => {
     const table=document.getElementById('table');
     peopleArray.forEach(people => table.append(createFilm(people.name, people.height,people.mass, people.gender)))
     return table
--- homework_11/table_tp/table.js
+++ homework_11/table_tp/table.js
@@ -2,8 +2,9 @@
 
 import {swapi} from './api/swapi.js' // Импорт из другого модуля
 import {swapi as justApi} from './api/swapi.js' // Импорт из другого модуля под другим имененм
-import {createApiPoints} from './components/swapiComponents.js'
-import {createPeopleList} from "./components/swapiComponents.js";
+// import {createApiPoints} from './src/main.js'
+import {createPeopleList} from "./dist/main.js"
+
 
 // swapi.getMain((obj) => document.body.append(createApiPoints(obj)), console.error)
 justApi.getPeoples(console.log)
