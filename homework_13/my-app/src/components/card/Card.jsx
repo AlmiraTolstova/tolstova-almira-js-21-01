f8eb0d5d1f74b419e0c71c4b650f88adb362f205
AlmiraTolstova <almiratolstova@gmail.com>
2021-11-09 21:09:29 +0300

    Компоненты карточек рыб

--- homework_13/my-app/src/components/card/Card.jsx
+++ homework_13/my-app/src/components/card/Card.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
-import './Comment.css'
+import './Card.css'
 
-export class Comment extends React.Component{
+export class Card extends React.Component{
     render() {
         return <div className="block2__column2-grid-card flex">
             <div className="block2__column2-grid-img"></div>
             <div className="block2__column2-grid-buttons">
-                <a className="block2__column2-grid-a" href="">Палтус</a>
+                <a className="block2__column2-grid-a" >{this.props.name}</a>
                 <button className="block2__column2-grid-btn">Купить</button>
             </div>
         </div>
