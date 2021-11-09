7337098d56d829b1f163629c562560f784989104
AlmiraTolstova <almiratolstova@gmail.com>
2021-11-09 11:48:17 +0300

    Запущенный react

---
+++ homework_13/Repeatable.js
@@ -0,0 +1,26 @@
+// class Repeatable extends React.Component {
+//     render() {
+//         return React.createElement('div', {
+//                 className: 'block2__column2-card',
+//             },
+//             'HTML')
+//     }
+// }
+
+// class  Repetable extends React.Component{
+//     render(){
+//         return <div className="block2__column2-cards">
+//         <div className="block2__column2-card">
+//             <a className="block2__column2-card-a" href="">Замороженные рыбы</a>
+//             <p>Мы заморозили рыбов для вас</p>
+//         </div>
+//         <div className="block2__column2-card">
+//             <a className="block2__column2-card-a" href="">Живые рыбы</a>
+//             <p>На кухню или на разведение</p>
+//         </div>
+//         </div>
+//     }
+// }
+// ReactDOM.render(
+//     React.createElement(Repeatable),document.getElementById('block2__cards'))
+// )
